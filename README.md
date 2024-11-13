6a6afc71d3120b133f57de42c30cf06413d96bda
Samuel Gebru <smaklilu90@gmail.com>
2024-11-13 12:44:07 -0500

    added content in MD file and more tests

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Asana_Automation
+# Asana_Automation Test Automation Framework
+
+A test automation framework built using Playwright for end-to-end testing of web applications. This framework supports parallel test execution, multiple browser testing.
+
+## Prerequisites
+
+Before you begin, ensure that you have the following installed:
+
+- Node.js (version >=16)
+- npm (Node Package Manager)
+
+## Installation
+
+1. Clone the repository to your local machine:
+   ```bash
+   git clone https://github.com/smaklilu90/Asana_Automation.git
+   ```
+
+2. Navigate to the project directory:
+   ```bash
+   cd Asna_Automation
+   ```
+
+3. Install the dependencies:
+   ```bash
+   npm install
+   ```
+
+## Running Tests
+
+To run the tests, use the following command:
+
+```bash
+npx playwright test
+```
+
+### Running Tests in Specific Browser
+
+You can run tests on a specific browser using the `--project` flag:
+
+```bash
+npx playwright test --project=firefox
+```
+
+### Running Tests in Parallel
+
+To run tests in parallel across multiple workers, use the `--workers` flag:
+
+```bash
+npx playwright test --workers=4
+```
+
+
--- tests/mainTest.spec.js
+++ tests/mainTest.spec.js
@@ -27,7 +27,7 @@ test('Validate Draft project brief card', async ({ page }) => {
     //validate visabilty of 'Draft Project brief' card
     expect(await page.locator(xpathTitle).isVisible()).toBe(true);
 
-    //Validate visbilty of tags under 'Draft Project brief' card
+    //Validate visbilty of tags in 'Draft Project brief' card
     expect(await page.locator(xpathNonPriority).isVisible()).toBe(true);
     expect(await page.locator(xpathOnTrack).isVisible()).toBe(true);
 
@@ -46,7 +46,7 @@ test('Validate Scedule kickoff meeting card', async ({ page }) => {
     expect(await page.locator(xpathTitle).isVisible()).toBe(true);
 
 
-    //Validate visbilty of tags under 'Draft Project brief' card
+    //Validate visbilty of tags in 'Draft Project brief' card
     expect(await page.locator(xpathMedium).isVisible()).toBe(true);
     expect(await page.locator(xpathRisk).isVisible()).toBe(true);
 });
@@ -64,7 +64,78 @@ test('Validate Share timeline with teammates card', async ({ page }) => {
     expect(await page.locator(xpathTitle).isVisible()).toBe(true);
 
 
-    //Validate visbilty of tags under 'Share timeline with teammates' card
+    //Validate visbilty of tags in 'Share timeline with teammates' card
     expect(await page.locator(xpathHigh).isVisible()).toBe(true);
     expect(await page.locator(xpathTrack).isVisible()).toBe(true);
+});
+
+test('Validate laptop setup for new hire', async ({ page }) =>{
+    await page.waitForLoadState('load');
+    await page.getByLabel('Work Requests').click();
+    // Generate the XPath
+    const xpathTitle = xpathCardTitle('1', '[Example] Laptop setup for new hire');
+    const xpathMedium = xpathCardTag('1', '[Example] Laptop setup for new hire', 'Medium priority');
+    const xpathlow = xpathCardTag('1', '[Example] Laptop setup for new hire', 'Low effort');
+    const xpathHardware = xpathCardTag('1', '[Example] Laptop setup for new hire', 'New hardware');
+    const xpathNotStarted = xpathCardTag('1', '[Example] Laptop setup for new hire', 'Not Started');
+
+    await page.locator(xpathTitle).waitFor({ state: 'visible', timeout: 5000 });
+    //validate visabilty of 'Schedule kickoff meeting' card
+    expect(await page.locator(xpathTitle).isVisible()).toBe(true);
+
+
+    //Validate visbilty of tags in '[Example] Laptop setup for new hire' card
+    expect(await page.locator(xpathMedium).isVisible()).toBe(true);
+    expect(await page.locator(xpathlow).isVisible()).toBe(true);
+    expect(await page.locator(xpathHardware).isVisible()).toBe(true);
+    expect(await page.locator(xpathNotStarted).isVisible()).toBe(true);
+
+});
+
+test('Validate Password not working', async({page}) => {
+
+    await page.waitForLoadState('load');
+    await page.getByLabel('Work Requests').click();
+    // Generate the XPath
+    const xpathTitle = xpathCardTitle('3', '[Example] Password not working');
+    const xpathPriority = xpathCardTag('3', '[Example] Password not working', 'Low priority');
+    const xpathlow = xpathCardTag('3', '[Example] Password not working', 'Low effort');
+    const xpathPassword = xpathCardTag('3', '[Example] Password not working', 'Password reset');
+    const xpathWaiting = xpathCardTag('3', '[Example] Password not working', 'Waiting');
+
+    await page.locator(xpathTitle).waitFor({ state: 'visible', timeout: 5000 });
+    //validate visabilty of 'Schedule kickoff meeting' card
+    expect(await page.locator(xpathTitle).isVisible()).toBe(true);
+
+
+    //Validate visbilty of tags in '[Example] Password not working' card
+    expect(await page.locator(xpathPriority).isVisible()).toBe(true);
+    expect(await page.locator(xpathlow).isVisible()).toBe(true);
+    expect(await page.locator(xpathPassword).isVisible()).toBe(true);
+    expect(await page.locator(xpathWaiting).isVisible()).toBe(true);
+
+});
+
+test('Validate New Keycard for Daniela V', async({page}) => {
+
+    await page.waitForLoadState('load');
+    await page.getByLabel('Work Requests').click();
+    // Generate the XPath
+    const xpathTitle = xpathCardTitle('4', '[Example] New keycard for Daniela V');
+    const xpathHardware = xpathCardTag('4', '[Example] New keycard for Daniela V', 'New hardware');
+    const xpathlow = xpathCardTag('4', '[Example] New keycard for Daniela V', 'Low effort');
+    const xpathHigh = xpathCardTag('4', '[Example] New keycard for Daniela V', 'High priority');
+    const xpathDone = xpathCardTag('4', '[Example] New keycard for Daniela V', 'Done');
+
+    await page.locator(xpathTitle).waitFor({ state: 'visible', timeout: 5000 });
+    //validate visabilty of 'Schedule kickoff meeting' card
+    expect(await page.locator(xpathTitle).isVisible()).toBe(true);
+
+
+    //Validate visbilty of tags in '[Example] New keycard for Daniela V' card
+    expect(await page.locator(xpathHardware).isVisible()).toBe(true);
+    expect(await page.locator(xpathlow).isVisible()).toBe(true);
+    expect(await page.locator(xpathHigh).isVisible()).toBe(true);
+    expect(await page.locator(xpathDone).isVisible()).toBe(true);
+
 });
